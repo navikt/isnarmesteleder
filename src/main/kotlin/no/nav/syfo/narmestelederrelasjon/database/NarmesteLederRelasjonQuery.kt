1d6b3f5cb01d5b32c82aa35cb665427bbe90ec76
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-10-05 21:58:18 +0200

    Limit each processing of virksomhetsnavn to 10k rows

--- src/main/kotlin/no/nav/syfo/narmestelederrelasjon/database/NarmesteLederRelasjonQuery.kt
+++ src/main/kotlin/no/nav/syfo/narmestelederrelasjon/database/NarmesteLederRelasjonQuery.kt
@@ -70,6 +70,7 @@ const val queryGetNarmesteLederRelasjonWithoutVirksomhetsnavn =
     SELECT *
     FROM NARMESTE_LEDER_RELASJON
     WHERE virksomhetsnavn IS NULL
+    LIMIT 10000
     """
 
 fun DatabaseInterface.getNarmesteLederRelasjonWithoutVirksomhetsnavn(): List<PNarmesteLederRelasjon> {
