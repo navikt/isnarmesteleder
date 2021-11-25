c6976305340fb591d586a22f8e3e292fdce52751
John Martin Lindseth <john.martin.lindseth@nav.no>
2021-11-25 14:35:23 +0100

    Add api for system calls
    Send status Forbidden when system consumer is denied access.
    Give access to syfomoteadmin.

--- src/main/kotlin/no/nav/syfo/application/ApplicationEnvironment.kt
+++ src/main/kotlin/no/nav/syfo/application/ApplicationEnvironment.kt
@@ -5,6 +5,7 @@ import io.ktor.application.*
 data class Environment(
     val azureAppClientId: String = getEnvVar("AZURE_APP_CLIENT_ID"),
     val azureAppClientSecret: String = getEnvVar("AZURE_APP_CLIENT_SECRET"),
+    val azureAppPreAuthorizedApps: String = getEnvVar("AZURE_APP_PRE_AUTHORIZED_APPS"),
     val azureAppWellKnownUrl: String = getEnvVar("AZURE_APP_WELL_KNOWN_URL"),
     val azureOpenidConfigTokenEndpoint: String = getEnvVar("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"),
 
@@ -36,6 +37,11 @@ data class Environment(
 
     val syfotilgangskontrollClientId: String = getEnvVar("SYFOTILGANGSKONTROLL_CLIENT_ID"),
     val syfotilgangskontrollUrl: String = getEnvVar("SYFOTILGANGSKONTROLL_URL"),
+
+    val syfomoteadminApplicationName: String = "syfomoteadmin",
+    val systemAPIAuthorizedConsumerApplicationNameList: List<String> = listOf(
+        syfomoteadminApplicationName,
+    ),
 ) {
     fun jdbcUrl(): String {
         return "jdbc:postgresql://$isnarmestelederDbHost:$isnarmestelederDbPort/$isnarmestelederDbName"
--- src/main/kotlin/no/nav/syfo/application/api/authentication/TokenAuth.kt
+++ src/main/kotlin/no/nav/syfo/application/api/authentication/TokenAuth.kt
@@ -1,3 +1,10 @@
 package no.nav.syfo.application.api.authentication
 
+import com.auth0.jwt.JWT
+
 const val JWT_CLAIM_NAVIDENT = "NAVident"
+const val JWT_CLAIM_AZP = "azp"
+
+fun getConsumerClientId(token: String): String =
+    JWT.decode(token).claims[JWT_CLAIM_AZP]?.asString()
+        ?: throw IllegalArgumentException("Claim AZP was not found in token")
