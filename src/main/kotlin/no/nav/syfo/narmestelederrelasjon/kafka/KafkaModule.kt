42d96b1feceb390ad617aee0ae4f3923c2affada
Anders Rognstad <anders.rognstad@nav.no>
2022-05-12 11:10:54 +0200

    move to backgroundtask + use unbounded dispatcher

--- src/main/kotlin/no/nav/syfo/narmestelederrelasjon/kafka/KafkaModule.kt
+++ src/main/kotlin/no/nav/syfo/narmestelederrelasjon/kafka/KafkaModule.kt
@@ -1,27 +1,9 @@
 package no.nav.syfo.narmestelederrelasjon.kafka
 
-import kotlinx.coroutines.*
 import no.nav.syfo.application.ApplicationState
 import no.nav.syfo.application.database.DatabaseInterface
 import no.nav.syfo.application.kafka.KafkaEnvironment
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-
-private val log: Logger = LoggerFactory.getLogger("no.nav.syfo.narmestelederrelasjon.kafka")
-
-fun launchBackgroundTask(
-    applicationState: ApplicationState,
-    action: suspend CoroutineScope.() -> Unit
-): Job = GlobalScope.launch {
-    try {
-        action()
-    } catch (ex: Exception) {
-        log.error("Exception received while launching background task. Terminating application.", ex)
-    } finally {
-        applicationState.alive = false
-        applicationState.ready = false
-    }
-}
+import no.nav.syfo.application.launchBackgroundTask
 
 fun launchKafkaTask(
     applicationState: ApplicationState,
