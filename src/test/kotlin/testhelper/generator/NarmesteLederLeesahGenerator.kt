3c4f92dcfdec554ee5834b47442d2c9f27b0138d
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2022-09-30 10:08:24 +0200

    Bugfix lederbytte (#66)

--- src/test/kotlin/testhelper/generator/NarmesteLederLeesahGenerator.kt
+++ src/test/kotlin/testhelper/generator/NarmesteLederLeesahGenerator.kt
@@ -10,24 +10,28 @@ import testhelper.UserConstants.NARMESTELEDER_AKTIV_FOM
 import testhelper.UserConstants.NARMESTELEDER_EPOST
 import testhelper.UserConstants.NARMESTELEDER_PERSONIDENTNUMBER
 import testhelper.UserConstants.NARMESTELEDER_TELEFON
+import java.time.LocalDate
 import java.time.OffsetDateTime
 import java.util.*
 
 fun generateNarmesteLederLeesah(
+    narmesteLederId: UUID = UUID.randomUUID(),
     arbeidstakerPersonIdentNumber: PersonIdentNumber = ARBEIDSTAKER_FNR,
     narmestelederPersonIdentNumber: PersonIdentNumber = NARMESTELEDER_PERSONIDENTNUMBER,
     status: String? = NY_LEDER,
     timestamp: OffsetDateTime = OffsetDateTime.now().minusDays(5),
     virksomhetsnummer: Virksomhetsnummer = UserConstants.VIRKSOMHETSNUMMER_DEFAULT,
+    aktivFom: LocalDate = NARMESTELEDER_AKTIV_FOM,
+    aktivTom: LocalDate? = null,
 ) = NarmesteLederLeesah(
-    narmesteLederId = UUID.randomUUID(),
+    narmesteLederId = narmesteLederId,
     fnr = arbeidstakerPersonIdentNumber.value,
     orgnummer = virksomhetsnummer.value,
     narmesteLederFnr = narmestelederPersonIdentNumber.value,
     narmesteLederTelefonnummer = NARMESTELEDER_TELEFON,
     narmesteLederEpost = NARMESTELEDER_EPOST,
-    aktivFom = NARMESTELEDER_AKTIV_FOM,
-    aktivTom = null,
+    aktivFom = aktivFom,
+    aktivTom = aktivTom,
     arbeidsgiverForskutterer = null,
     timestamp = timestamp,
     status = status,
